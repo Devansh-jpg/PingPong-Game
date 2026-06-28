abee63536128fec0fcf777986f27ea6e88936773
Devansh-jpg <1105devansh@gmail.com>
2026-06-28 17:41:23 +0530

    scoring and graphics

--- pingpong-game/main.cpp
+++ pingpong-game/main.cpp
@@ -1,153 +1,155 @@
-#include<iostream> 
+#include<iostream>
 #include<raylib.h>
 
 using namespace std;
 
+Color Green = Color{38, 185, 154, 255};
+Color Dark_Green = Color{20, 160, 133, 255};
+Color Light_Green = Color{129, 204, 184, 255};
+Color Yellow = Color{243, 213, 91, 255};
+
+int player_score = 0;
+int cpu_score = 0;
+
 class Ball {
     public:
-    float x,y;
+    float x, y;
     int speed_x, speed_y;
     int radius;
 
     void Draw(){
-         DrawCircle(x, y, radius, WHITE);
-
+        DrawCircle(x, y, radius, Yellow);
     }
+
     void Update(){
-        x+=speed_x;
-        y+= speed_y;
-        // reflection
-        if(y+ radius>= GetScreenHeight() || y-radius<=0){
-            speed_y*=-1;
+        x += speed_x;
+        y += speed_y;
 
+        // bounce off top and bottom
+        if(y + radius >= GetScreenHeight() || y - radius <= 0){
+            speed_y *= -1;
         }
-        if(x+radius>=GetScreenWidth()||x-radius<=0){
-            speed_x*=-1;
-
+        // cpu scores when ball exits right
+        if(x + radius >= GetScreenWidth()){
+            cpu_score++;
+            ResetBall();
+        }
+        // player scores when ball exits left
+        if(x - radius <= 0){
+            player_score++;
+            ResetBall();
         }
     }
 
+    void ResetBall(){
+        x = GetScreenWidth() / 2;
+        y = GetScreenHeight() / 2;
 
+        int speed_choices[2] = {-1, 1};
+        speed_x *= speed_choices[GetRandomValue(0, 1)];
+        speed_y *= speed_choices[GetRandomValue(0, 1)];
+    }
 };
-class Paddle{
+
+class Paddle {
     protected:
     void LimitMovement(){
-        if(y<=0){
-            y=0;
-        }
-        if(y>=GetScreenHeight()-height){
-            y= GetScreenHeight()-height;
-        }
-
+        if(y <= 0) y = 0;
+        if(y + height >= GetScreenHeight()) y = GetScreenHeight() - height;
     }
+
     public:
-    float x,y;
-    float width,height;
+    float x, y;
+    float width, height;
     int speed;
 
     void Draw(){
-        DrawRectangle(x,y, width, height, WHITE);
-
+        DrawRectangleRounded(Rectangle{x, y, width, height}, 0.8, 0, WHITE);
     }
+
     void Update(){
-        
+        if(IsKeyDown(KEY_UP))   y -= speed;
+        if(IsKeyDown(KEY_DOWN)) y += speed;
         LimitMovement();
-        if(IsKeyDown(KEY_UP)){
-            y -= speed;
-
-        }
-        if(IsKeyDown(KEY_DOWN)){
-            y+=speed;
-        }
     }
-
 };
 
-class CpuPaddle: public Paddle{
+class CpuPaddle: public Paddle {
     public:
-        void Update( float ball_y){
-            if(y+height/2> ball_y){
-                y=y-speed;
-            }
-            if(y+height/2<ball_y){
-                y=y+speed;
-            }
-            LimitMovement();
-        }
-    
+    void Update(float ball_y){
+        if(y + height / 2 > ball_y) y -= speed;
+        if(y + height / 2 <= ball_y) y += speed;
+        LimitMovement();
+    }
 };
+
 Ball ball;
 Paddle player;
 CpuPaddle cpu;
 
 int main(){
-    cout<<"Starting the game"<< endl;
-    const float screen_width=1280;
-    const float screen_height= 800;
-    const int bat_size= 120;
-    const int padding = 10;
-    const int bat_width= 25;
-    InitWindow(screen_width, screen_height, "My Pong Game");
+    cout << "Starting the game" << endl;
+    const int screen_width = 1280;
+    const int screen_height = 800;
+    InitWindow(screen_width, screen_height, "My Pong Game!");
     SetTargetFPS(60);
 
-    ball.radius=20;
-    ball.x=screen_width/2;
-    ball.y=screen_height/2;
-    ball.speed_x=7;
-    ball.speed_y=7;
+    ball.radius = 20;
+    ball.x = screen_width / 2;
+    ball.y = screen_height / 2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
 
-    player.x=padding;
-    player.y=(screen_height-bat_size)/2;
-    player.width=bat_width;
-    player.height=bat_size;
-    player.speed=6;
+    // player on the right, controlled by UP/DOWN keys
+    player.width = 25;
+    player.height = 120;
+    player.x = screen_width - player.width - 10;
+    player.y = screen_height / 2 - player.height / 2;
+    player.speed = 6;
 
-    cpu.height=  bat_size;
-    cpu.width=bat_width;
-    cpu.x=screen_width-bat_width-padding;
-    cpu.y=(screen_height-bat_size)/2;
-    cpu.speed=6;
+    // cpu on the left, AI-controlled
+    cpu.width = 25;
+    cpu.height = 120;
+    cpu.x = 10;
+    cpu.y = screen_height / 2 - cpu.height / 2;
+    cpu.speed = 6;
 
-    while(WindowShouldClose()==false){
+    while(WindowShouldClose() == false){
         BeginDrawing();
-        // update
+
+        // Update
         ball.Update();
         player.Update();
         cpu.Update(ball.y);
 
-        //collision checkcing
-        if(CheckCollisionCircleRec(Vector2{ball.x,ball.y},ball.radius,Rectangle{player.x,player.x,player.width,player.height})){
-            ball.speed_x *=-1;
+        // Collision detection
+        if(CheckCollisionCircleRec({ball.x, ball.y}, ball.radius,
+            {player.x, player.y, player.width, player.height})){
+            ball.speed_x *= -1;
         }
-         if(CheckCollisionCircleRec(Vector2{ball.x,ball.y},ball.radius,Rectangle{cpu.x,cpu.x,cpu.width,cpu.height})){
-            ball.speed_x *=-1;
+        if(CheckCollisionCircleRec({ball.x, ball.y}, ball.radius,
+            {cpu.x, cpu.y, cpu.width, cpu.height})){
+            ball.speed_x *= -1;
         }
 
-        //drawing
-        ClearBackground(BLACK);
+        // Draw background
+        ClearBackground(Dark_Green);
+        DrawRectangle(screen_width / 2, 0, screen_width / 2, screen_height, Green);
+        DrawCircle(screen_width / 2, screen_height / 2, 150, Light_Green);
+        DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
 
+        // Draw game objects
         ball.Draw();
-        player.Draw();
         cpu.Draw();
-        
+        player.Draw();
 
-        // DrawRectangle(padding,(screen_height-bat_size)/2,bat_width, bat_size, WHITE);
-        
-        // DrawRectangle(screen_width-bat_width-padding,(screen_height-bat_size)/2,bat_width, bat_size, WHITE);
-        DrawLine(screen_width/2,0, screen_width/2,screen_height,WHITE);
-       
-
-
+        // Draw scores
+        DrawText(TextFormat("%i", cpu_score),    screen_width / 4 - 20,     20, 80, WHITE);
+        DrawText(TextFormat("%i", player_score), 3 * screen_width / 4 - 20, 20, 80, WHITE);
 
         EndDrawing();
-
     }
 
-
     CloseWindow();
-
-    
-
-
     return 0;
 }
