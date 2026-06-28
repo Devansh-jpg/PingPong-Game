71eda4e9b5ec5345fa34d1d881f3be42fe066b81
Devansh-jpg <1105devansh@gmail.com>
2026-06-28 16:42:15 +0530

    static figures

---
+++ PingPong Game.md
@@ -0,0 +1,2 @@
+static figgures 
+![[Pasted image 20260628164140.png]]
--- pingpong-game/main.cpp
+++ pingpong-game/main.cpp
@@ -7,10 +7,18 @@ int main(){
     cout<<"Starting the game"<< endl;
     const int screen_width=1280;
     const int screen_height= 800;
+    const int bat_size= 120;
+    const int padding = 10;
+    const int bat_width= 25;
     InitWindow(screen_width, screen_height, "My Pong Game");
     SetTargetFPS(60);
     while(WindowShouldClose()==false){
         BeginDrawing();
+
+        DrawRectangle(padding,(screen_height-bat_size)/2,bat_width, bat_size, WHITE);
+        DrawRectangle(screen_width-bat_width-padding,(screen_height-bat_size)/2,bat_width, bat_size, WHITE);
+        DrawLine(screen_width/2,0, screen_width/2,screen_height,WHITE);
+        DrawCircle(screen_width/2, screen_height/2, 20, WHITE);
 
 
 
